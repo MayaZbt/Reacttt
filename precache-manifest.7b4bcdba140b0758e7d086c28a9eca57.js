85df2ce583573681e9e853cba38c52d72b51e78d
Maya <mayazbt@gmail.com>
2020-07-13 22:14:48 +0300

    Updates

---
+++ precache-manifest.7b4bcdba140b0758e7d086c28a9eca57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f19db5f6a0142b4100f0c9d1c608871",
+    "url": "/Reacttt/index.html"
+  },
+  {
+    "revision": "090fc9859f00e6068ab5",
+    "url": "/Reacttt/static/css/main.0229f20c.chunk.css"
+  },
+  {
+    "revision": "2bad0aa8fed9d2b7e9fa",
+    "url": "/Reacttt/static/js/2.9b49ef18.chunk.js"
+  },
+  {
+    "revision": "090fc9859f00e6068ab5",
+    "url": "/Reacttt/static/js/main.7a696123.chunk.js"
+  },
+  {
+    "revision": "21350a96d51bebad4c9c",
+    "url": "/Reacttt/static/js/runtime~main.47d9aceb.js"
+  }
+]);
